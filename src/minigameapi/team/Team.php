1fcc9e1fb5ffbac01b27ddc065e9e776c93aa5f8
root <root@teclast.localdomain>
2018-05-24 01:12:18 +0900

    functions!

--- src/minigameapi/team/Team.php
+++ src/minigameapi/team/Team.php
@@ -5,31 +5,46 @@ use pocketmine\Player;
 use pocketmine\Server;
 
 class Team {
-	private $maxPlayers = '0';
-	private $minPlayers = '0';
+	private $maxPlayers = -1;
+	private $minPlayers = 0;
 	private $teamName;
 	private $players = [];
+	
+	public function __construct(string $teamName, Player $firstPlayer, int $maxPlayers = -1, int $minPlayers = 0) {
+		$this->setMaxPlayers($maxPlayers);
+		$this->setMinPlayers($minPlayers);
+		$this->setTeamName($teamName);
+		$this->addPlayer($firstPlayer);
+	}
 	public function getMaxPlayers() : int{
 		return $this->maxPlayers;
 	}
 	public function setMaxPlayers(int $maxPlayers) : bool{
 		if(count($this->players) > $maxPlayers) {
-			
+			Server::getInstance()->getLogger()->error('$maxPlayers has to be smaller than count of players in team');
+			return FALSE;
 		}
-	}
-	public function __construct(string $teamName, Player $firstPlayer, int $maxPlayers = 0, int $minPlayers = 0) {
 		$this->maxPlayers = $maxPlayers;
+		return true;
+	}
+	public function getMinPlayers() : int{
+		return $this->minPlayers;
+	}
+	public function setMinPlayers(int $minPlayers) {
 		$this->minPlayers = $minPlayers;
+		return;
+	}
+	public function setTeamName(string $teamName) {
 		$this->teamName = $teamName;
-		$this->players[] = $firstPlayer;
+		return;
 	}
 	public function addPlayer(Player $player) : bool{
 		if(count($this->players) == $this->maxPlayers) {
 			Server::getInstance()->getLogger()->error('adding new player on full team');
 			return false;
 		}
+		$this->removeExistsPlayers($player);
 		$this->players[] = $player;
-		//TODO remove exists players
 		return true;
 	}
 	public function removeExistsPlayers(Player $player) {
