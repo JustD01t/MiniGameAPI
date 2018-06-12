acadaea0b66f1aa2a547c6a33e58f3b35d30edb9
djdisodo <djdisodo@gmail.com>
2018-06-12 09:02:23 +0000

    fix typo

--- src/minigameapi/Team.php
+++ src/minigameapi/Team.php
@@ -19,7 +19,7 @@ class Team {
 	public function getMaxPlayers() : int{
 		return $this->maxPlayers;
 	}
-	public function setMaxPlayer(int $maxPlayers) : bool{
+	public function setMaxPlayers(int $maxPlayers) : bool{
 		if(count($this->players) > $maxPlayers) {
 			Server::getInstance()->getLogger()->error('$maxPlayers has to be smaller than count of players in team');
 			return FALSE;
@@ -50,7 +50,7 @@ class Team {
 	public function getPlayers() : array {
 		return $this->players;
 	}
-	public function removesPlayers(Player $player) {
+	public function removesPlayer(Player $player) {
 		foreach ($this->players as $key => $pl) {
 			$pl instanceof Player;
 			if($player->getName() == $pl->getName()) {
