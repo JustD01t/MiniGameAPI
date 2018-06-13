b66f807252c505c410f68e38c6480a13f9a420ce
djdisodo <djdisodo@gmail.com>
2018-06-13 20:12:03 +0900

    Update GameManager.php

--- src/minigameapi/GameManager.php
+++ src/minigameapi/GameManager.php
@@ -4,5 +4,23 @@ namespace minigameapi;
 
 class GameManager {
   private $games = [];
-  
+  private $miniGameApi;
+  public function __construct(MiniGameApi $miniGameApi) {
+    $this->miniGameApi = $miniGameApi;
+  }
+  public function submitGame(Game $game) : ?int{
+    if
+    $game->setGameManager($this);
+    $game->setGameId(array_pop(array_keys($this->games)));
+    $this->games[] = $game;
+    return;
+  }
+  public function getGames() : array{
+    return $this->games()
+  }
+  public function getGameId(string $gameTitle) : ?int{
+    foreach($this->getGames() as $gameId => $game) {
+      if($game->getTitle() == $gameTitle) return $gameId;
+    }
+  }   
 }
