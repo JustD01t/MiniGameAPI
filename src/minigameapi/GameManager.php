54f813688670c87f3dde34a6664f01449838ba24
djdisodo <djdisodo@gmail.com>
2018-06-13 22:04:50 +0900

    Update GameManager.php

--- src/minigameapi/GameManager.php
+++ src/minigameapi/GameManager.php
@@ -8,19 +8,33 @@ class GameManager {
   public function __construct(MiniGameApi $miniGameApi) {
     $this->miniGameApi = $miniGameApi;
   }
-  public function submitGame(Game $game) : ?int{
-    if
-    $game->setGameManager($this);
-    $game->setGameId(array_pop(array_keys($this->games)));
+  public function broadcastMessageToGames(string $message){
+    foreach($this->getGames() as $game) {
+      $game->broadcastMessage($message);
+    }
+  }
+  public function submitGame(Game $game) {
+    if(!is_null($this->getGame($game->getName()))) return;
     $this->games[] = $game;
-    return;
   }
   public function getGames() : array{
     return $this->games()
   }
-  public function getGameId(string $gameTitle) : ?int{
-    foreach($this->getGames() as $gameId => $game) {
-      if($game->getTitle() == $gameTitle) return $gameId;
+  public function getGame(string $gameName) : ?Game{
+    foreach($this->getGames() as $game) {
+      if($game->getName() == $gameName) return $game;
     }
-  }   
+  }
+  public function getTeams() : array {
+    $result = [];
+    foreach($this->getGames() as $game) {
+      $result = array_merge($result,$game->getTeams();
+    }
+    return $result;
+  }
+  public function removePlayer(Player $player){
+    foreach($this->getGames() as $game) {
+      $game->removePlayer($player);
+    }
+  }
 }
