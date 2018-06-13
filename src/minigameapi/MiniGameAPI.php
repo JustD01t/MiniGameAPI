155c4ed4043371341ed8e35a001316c89de53436
djdisodo <djdisodo@gmail.com>
2018-06-13 19:44:39 +0900

    Update MiniGameAPI.php

--- src/minigameapi/MiniGameAPI.php
+++ src/minigameapi/MiniGameAPI.php
@@ -5,5 +5,13 @@ namespace minigameapi;
 use pocketmine\plugin\PluginBase;
 
 class MiniGameAPI extends PluginManager {
+  private $gameManager;
+  private $gameId;
+  public function setGameId(int $id){
+    $this->gameId = $id;
+  }
   static function getInstance(); //TODO 구현
+  public function getGameManager() : GameManager{
+    return $this->gameManager;
+  }
 }
