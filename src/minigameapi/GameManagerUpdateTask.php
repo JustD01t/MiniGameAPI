6c07100b2910eafb5fecc851df033d6f3c985ad6
djdisodo <djdisodo@gmail.com>
2018-06-21 00:45:45 +0900

    Update GameManagerUpdateTask.php

--- src/minigameapi/GameManagerUpdateTask.php
+++ src/minigameapi/GameManagerUpdateTask.php
@@ -4,5 +4,11 @@ namespace minigameapi;
 use pocketmine\sheduler\Task;
 
 class GameManagerUpdateTask extends Task {
-
+	private $gameManager;
+	public function __construct(GameManager $gameManager) {
+		$this->gameManager = $gameManager;
+	}
+	public function onRun(int $currentTick) {
+		$this->gameManager->update();
+	}
 }
