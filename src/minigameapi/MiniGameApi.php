bfec17b1d9281752d6d994991d6590cec699e708
djdisodo <djdisodo@gmail.com>
2018-06-21 00:54:33 +0900

    Update MiniGameApi.php

--- src/minigameapi/MiniGameApi.php
+++ src/minigameapi/MiniGameApi.php
@@ -5,13 +5,21 @@ namespace minigameapi;
 use pocketmine\plugin\PluginBase;
 
 class MiniGameApi extends PluginManager {
-  private $gameManager;
-  private $gameId;
-  public function setGameId(int $id){
-    $this->gameId = $id;
-  }
-  static function getInstance(); //TODO 구현
-  public function getGameManager() : GameManager{
-    return $this->gameManager;
-  }
+	private $gameManager;
+	private $gameId;
+	private static $instance = null;
+	public function onLoad() {
+		self::$instance = $this;
+	}
+	public function getInstance() : MiniGameApi{
+		return self::$instance;
+	}
+	public function onEnable() {
+		$this->gameManager = new GameManager($this);
+		$this->getScheduler()->scheduleRepeatingTask(new GameManagerUpdateTask($this->getGameManager));
+	}
+	static function getInstance(); //TODO 구현
+	public function getGameManager() : GameManager{
+  		return $this->gameManager;
+	}
 }
