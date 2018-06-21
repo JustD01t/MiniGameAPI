db1009449dbc489b20a4244f6b97e35d8d0e3f39
djdisodo <djdisodo@gmail.com>
2018-06-22 00:47:01 +0900

    Update MiniGameApi.php

--- src/minigameapi/MiniGameApi.php
+++ src/minigameapi/MiniGameApi.php
@@ -15,10 +15,15 @@ class MiniGameApi extends PluginBase {
 		return self::$instance;
 	}
 	public function onEnable() {
+		@mkdir($this->getDataFolder());
+		$this->saveDefaultConfig();
 		$this->gameManager = new GameManager($this);
 		$this->getScheduler()->scheduleRepeatingTask(new GameManagerUpdateTask($this->getGameManager()), 0);
 	}
 	public function getGameManager() : GameManager{
   		return $this->gameManager;
+	}
+	public function format(string $message) : string{
+		
 	}
 }
