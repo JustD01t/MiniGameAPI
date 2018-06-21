b33cbfb057222f32a66bbac07a901d8c6d59a7d4
djdisodo <djdisodo@gmail.com>
2018-06-22 01:01:37 +0900

    Update GameManagerUpdateTask.php

--- src/minigameapi/GameManagerUpdateTask.php
+++ src/minigameapi/GameManagerUpdateTask.php
@@ -5,10 +5,12 @@ use pocketmine\scheduler\Task;
 
 class GameManagerUpdateTask extends Task {
 	private $gameManager;
-	public function __construct(GameManager $gameManager) {
+	private $updateCycle;
+	public function __construct(GameManager $gameManager, int $updateCycle) {
 		$this->gameManager = $gameManager;
+		$this->updateCycle = $updateCycle;
 	}
 	public function onRun(int $currentTick) {
-		$this->gameManager->update();
+		$this->gameManager->update($this->updateCycle);
 	}
 }
