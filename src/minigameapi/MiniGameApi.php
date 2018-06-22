97fdfc042334ca81fe549ffaefbe86d2bb382c2c
djdisodo <djdisodo@gmail.com>
2018-06-23 00:32:17 +0900

    Update MiniGameApi.php

--- src/minigameapi/MiniGameApi.php
+++ src/minigameapi/MiniGameApi.php
@@ -3,11 +3,13 @@
 namespace minigameapi;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\lang\BaseLang;
 
 class MiniGameApi extends PluginBase {
 	private $gameManager;
 	private $gameId;
 	private static $instance = null;
+	private $baseLang;
 	public function onLoad() {
 		self::$instance = $this;
 	}
@@ -19,11 +21,12 @@ class MiniGameApi extends PluginBase {
 		$this->saveDefaultConfig();
 		$this->gameManager = new GameManager($this);
 		$this->getScheduler()->scheduleRepeatingTask(new GameManagerUpdateTask($this->getGameManager()), 0);
+		$this->baseLang = new BaseLang($this->getConfig()->get('language') == 'auto' ? $this->getServer()->getProperty("settings.language") : $this->getConfig()->get('language'),$this->getDataFolder() . 'lang' . DIRECTORY_SEPARATOR);
+	}
+	public function getBaseLang() : BaseLang{
+		return $this->baseLang;
 	}
 	public function getGameManager() : GameManager{
   		return $this->gameManager;
-	}
-	public function format(string $message) : string{
-		
 	}
 }
