f710b8981a11f76fb712eebf45af6db95137c902
djdisodo <djdisodo@gmail.com>
2018-06-23 13:30:55 +0900

    Update PlayerData.php

--- src/minigameapi/PlayerData.php
+++ src/minigameapi/PlayerData.php
@@ -2,5 +2,17 @@
 namespace minigameapi;
 
 class PlayerData {
-
+	private $nameTag;
+	private $items = [];
+	private $armors = [];
+	public function __construct(Player $player) {
+		$this->nameTag = $player->getNameTag();
+		$this->items = $player->getInventory()->getContents(true);
+		$this->armors = $player->getArmorInventory()->getContents(true);
+	}
+	public function restore(Player $player) {
+		$player->setNameTag($this->nameTag);
+		$player->getInventory()->setContents($this->items,true);
+		$player->getInventory()->setContetns($this->items,true);
+	}
 }
