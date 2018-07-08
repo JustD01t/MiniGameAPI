21b9d3ff658a425dcae3ffc8709cdaaba1bb0090
djdisodo <djdisodo@gmail.com>
2018-07-09 03:36:28 +0900

    dafdafa

--- src/minigameapi/PlayerData.php
+++ src/minigameapi/PlayerData.php
@@ -1,7 +1,10 @@
 <?php
 namespace minigameapi;
 
+use pocketmine\level\Location;
+use pocketmine\level\Position;
 use pocketmine\Player;
+use pocketmine\Server;
 
 class PlayerData {
     private $location;
@@ -9,15 +12,18 @@ class PlayerData {
 	private $items = [];
 	private $armors = [];
 	public function __construct(Player $player) {
-	    $this->location = $player->getLocation();
+	    $this->location['level'] = $player->getLevel()->getFolderName();
+        $this->location['x'] = $player->getX();
+        $this->location['y'] = $player->getY();
+        $this->location['z'] = $player->getZ();
 		$this->nameTag = $player->getNameTag();
 		$this->items = $player->getInventory()->getContents(true);
 		$this->armors = $player->getArmorInventory()->getContents(true);
 	}
 	public function restore(Player $player) {
-	    $player->teleport($this->location);
+	    $player->teleport(new Position($this->location['x'],$this->location['y'],$this->location['z'],Server::getInstance()->getLevelByName($this->location['level'])));
 		$player->setNameTag($this->nameTag);
 		$player->getInventory()->setContents($this->items,true);
-		$player->getInventory()->setContetns($this->items,true);
+		$player->getArmorInventory()->setContetns($this->items,true);
 	}
 }
