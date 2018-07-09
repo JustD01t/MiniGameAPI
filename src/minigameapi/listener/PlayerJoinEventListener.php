29b93e2d6b7b536fdb6375f2501346ae8fbf9d43
djdisodo <djdisodo@gmail.com>
2018-07-09 22:57:56 +0900

    dsaf

--- src/minigameapi/listener/PlayerJoinEventListener.php
+++ src/minigameapi/listener/PlayerJoinEventListener.php
@@ -1,3 +1,17 @@
 <?php
 namespace minigameapi\listener;
-class PlayerJoinEventListener implements
+use minigameapi\MiniGameApi;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+
+class PlayerJoinEventListener implements Listener {
+    private $miniGameApi;
+    public function __construct(MiniGameApi $miniGameApi) {
+        $this->miniGameApi = $miniGameApi;
+    }
+    public function onPlayerJoin(PlayerJoinEvent $event) {
+        $data = $this->miniGameApi->getPlayerData($event->getPlayer()->getName());
+        if(is_null($data)) return;
+        $data->restore($event->getPlayer());
+    }
+}
