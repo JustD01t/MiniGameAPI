592318b460a14fcbb5758011e05f946c02ead651
djdisodo <djdisodo@gmail.com>
2018-07-15 06:11:29 +0900

    qq

--- src/minigameapi/event/MiniGameEvent.php
+++ src/minigameapi/event/MiniGameEvent.php
@@ -2,10 +2,12 @@
 namespace minigameapi\event;
 use minigameapi\Game;
 use pocketmine\event\Event;
+use pocketmine\event\plugin\PluginEvent;
 
-abstract class MiniGameEvent extends Event {
+abstract class MiniGameEvent extends PluginEvent {
 	private $game;
 	public function __construct(Game $game) {
+	    parent::__construct($game->getMiniGameApi());
 		$this->game = $game;
 	}
 	public function getGame() : Game {
