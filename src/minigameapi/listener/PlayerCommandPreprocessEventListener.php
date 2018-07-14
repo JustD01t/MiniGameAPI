9f92a4e8ee08c25c62544651a109f7587bf26e90
djdisodo <djdisodo@gmail.com>
2018-07-15 05:58:55 +0900

    hh

--- src/minigameapi/listener/PlayerCommandPreprocessEventListener.php
+++ src/minigameapi/listener/PlayerCommandPreprocessEventListener.php
@@ -13,7 +13,7 @@ class PlayerCommandPreprocessEventListener implements Listener {
 		if($event->isCancelled()) return;
 		$game = $this->gameManager->getJoinedGame($event->getPlayer());
 		if(is_null($game)) return;
-		$event->setCancelled(!$game->isAllowedCommand(substr(implode('.', array_map("stripslashes", str_getcsv($event->getMessage(), " ")))),1));
+		$event->setCancelled(!$game->isAllowedCommand(substr(implode('.', array_map("stripslashes", str_getcsv($event->getMessage(), " "))),1)));
 		if($event->isCancelled()) {
 			$event->getPlayer()->sendMessage($game->getPrefix() . $this->gameManager->getMiniGameApi()->getBaseLang()->translateString('commandMessage.commandNotAllowedInGame'));
 			$event->getPlayer()->sendMessage($this->gameManager->getMiniGameApi()->getBaseLang()->translateString('commandMessage.quitFirst', [$this->gameManager->getMiniGameApi()->getBaseLang()->translateString('command.quit.usage',[$this->gameManager->getMiniGameApi()->getBaseLang()->translateString('command.quit')]), $this->gameManager->getMiniGameApi()->getBaseLang()->translateString('command.miniGameApi.quit.usage',[$this->gameManager->getMiniGameApi()->getBaseLang()->translateString('command.miniGameApi'), $this->gameManager->getMiniGameApi()->getBaseLang()->translateString('command.miniGameApi.quit')])]));
